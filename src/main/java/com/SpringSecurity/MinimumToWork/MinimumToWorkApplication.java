6089658d24f2fb610a7a2272b73fd24e48a7b6e1
Jitox <jboggenmontanares@gmail.com>
2024-12-27 15:02:17 -0300

    Base project
    SpringBoot init project without configuration

---
+++ src/main/java/com/SpringSecurity/MinimumToWork/MinimumToWorkApplication.java
@@ -0,0 +1,13 @@
+package com.SpringSecurity.MinimumToWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimumToWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimumToWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringSecurity/MinimumToWork/MinimumToWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurity.MinimumToWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimumToWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
